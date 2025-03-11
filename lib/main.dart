b3ac8e3053343327e6ecb40ef5fd72783eaa2abe
dw0124 <doowone8650@naver.com>
2025-03-11 17:20:31 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Hello World!"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Hello"),
+            Text("Hello"),
+            Text("Hello"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
